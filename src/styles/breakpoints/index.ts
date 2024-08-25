21e237a82d286cec2fd693911addf6b5853209f3
cla6shade <cla6shade@gmail.com>
2024-08-25 16:47:16 +0900

    design: breakpoint 추가

---
+++ src/styles/breakpoints/index.ts
@@ -0,0 +1,9 @@
+export const BREAKPOINT_XS = '520px';
+
+export const BREAKPOINT_SM = '768px';
+
+export const BREAKPOINT_MD = '1024px';
+
+export const BREAKPOINT_LG = '1280px';
+
+export const breakPoints = [BREAKPOINT_XS, BREAKPOINT_SM, BREAKPOINT_MD, BREAKPOINT_LG];
