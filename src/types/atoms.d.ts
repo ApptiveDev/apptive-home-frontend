9929663dd52730dbbb7e9ca595f1fb11ea8e1a95
cla6shade <cla6shade@gmail.com>
2024-08-26 03:21:21 +0900

    refactor: breakpoint 관련 상수 분리

--- src/types/atoms.d.ts
+++ src/types/atoms.d.ts
@@ -1,14 +1,21 @@
 import { HTMLAttributes, ReactNode } from 'react';
 import { breakPoints } from '@styles/breakpoints';
+import { CSSObject } from '@emotion/react';
 
 export interface TextProps extends HTMLAttributes<HTMLParagraphElement> {
   children?: ReactNode;
   weight?: FontWeight;
-  responsiveSize?: { [key: BreakPointIndex]: string };
+  responsiveSize?: { [key: typeof breakPoints[string]]: string };
   defaultSize?: string;
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
 }
 
-export type BreakPointIndex = typeof breakPoints[number];
-
 export type FontWeight = number | 'medium' | 'bold' | 'regular' | 'lighter' | 'bolder';
+
+
+interface ResponsiveCSSObjects {
+  xs?: CSSObject;
+  sm?: CSSObject;
+  md?: CSSObject;
+  lg?: CSSObject;
+}
