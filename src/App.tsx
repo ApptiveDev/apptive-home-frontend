687e927692b2a4ebc51a846c080eacd115f900c0
cla6shade <cla6shade@github.com>
2024-08-27 16:45:03 +0900

    feat: add activity section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Page from '@components/templates/Page';
 import HeroSection from '@components/organisms/main/HeroSection';
+import ActivitySection from '@components/organisms/main/activity/ActivitySection';
 
 function App() {
   return (
     <Page>
       <HeroSection />
+      <ActivitySection />
     </Page>
   );
 }
