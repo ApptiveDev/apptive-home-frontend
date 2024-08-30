360186d267eaa01a33757a2a6938f251fb06cde6
harugi7 <gimme2721@gmail.com>
2024-08-30 17:47:44 +0900

    chore: 충돌 해결

--- src/components/organisms/semantic/Footer.tsx
+++ src/components/organisms/semantic/Footer.tsx
@@ -1,12 +1,12 @@
-import Container from "@/components/atoms/container/Container";
+import Container from '@/components/atoms/container/Container';
 import Image from '@components/atoms/image/Image';
 import headerLogo from '@assets/images/header-logo.svg';
 import linkIcon from '@assets/images/link-icon.svg';
-import { TextBody } from "@/components/atoms/text/TextFactory";
+import { TextBody } from '@/components/atoms/text/TextFactory';
 
 function Footer() {
-    return (
-        <Container size="full-width" justify="center" style={{backgroundColor: '#1B1B1B'}}>
+  return (
+        <Container size="full-width" justify="center" style={{ backgroundColor: '#1B1B1B' }}>
             <Container
               size={{ width: '100%', height: '320px' }}
               justify="space-between"
@@ -20,21 +20,21 @@ function Footer() {
                     <TextBody.Medium weight="bold"color="white">부산대학교 대표 IT 프로젝트 동아리</TextBody.Medium>
                 </Container>
                 <Container gap="12px" direction="column" >
-                    <a rel="stylesheet" href="" style={{textDecoration:'underline', color:'white'}}>
+                    <a rel="stylesheet" href="" style={{ textDecoration:'underline', color:'white' }}>
                         <Container gap="5px">
                             Notion
                         <Image src={linkIcon} alt="link icon" />
                         </Container>
                         
                     </a>
-                    <a rel="stylesheet" href="" style={{textDecoration:'underline', color:'white'}}>
+                    <a rel="stylesheet" href="" style={{ textDecoration:'underline', color:'white' }}>
                         <Container gap="5px">
                             Discord
                         <Image src={linkIcon} alt="link icon" />
                         </Container>
                         
                     </a>
-                    <a rel="stylesheet" href="" style={{textDecoration:'underline', color:'white'}}>
+                    <a rel="stylesheet" href="" style={{ textDecoration:'underline', color:'white' }}>
                         <Container gap="5px">
                             Instagram
                         <Image src={linkIcon} alt="link icon" />
@@ -46,7 +46,7 @@ function Footer() {
               </Container>
             
           </Container>
-    )
+  );
 }
 
 export default Footer;
