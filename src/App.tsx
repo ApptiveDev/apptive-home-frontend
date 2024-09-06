bae2fef42418ceeaa7c40be45dcc50990be3b33f
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-09-06 09:07:55 +0900

    feat: qna창 디자인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import HeroSection from '@components/organisms/main/HeroSection';
 import ActivitySection from '@components/organisms/main/activity/ActivitySection';
 import MainSection from './components/organisms/main/MainSection';
 import GallerySection from './components/gallery/GallerySection';
+import Qna from './components/organisms/qna/Qna';
 
 function App() {
   return (
     <Page>
+      <Qna />
       <HeroSection />
       <MainSection mode="dark" />
       <ActivitySection />
