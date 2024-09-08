510db59be8b5e63b58ab6491c46e9d1084c5a6bf
cla6shade <cla6shade@gmail.com>
2024-09-09 02:02:03 +0900

    refactor: 코드 스타일 관련 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Page from '@components/templates/Page';
 import HeroSection from '@components/organisms/main/HeroSection';
 import ActivitySection from '@components/organisms/main/activity/ActivitySection';
-import MainSection from './components/organisms/main/MainSection';
-import GallerySection from './components/gallery/GallerySection';
-import Qna from './components/organisms/qna/Qna';
+import MainSection from '@components/organisms/main/MainSection';
+import GallerySection from '@components/organisms/gallery/GallerySection';
+import Qna from '@components/organisms/qna/Qna';
 
 function App() {
 
--- src/components/organisms/qna/FloatingButton.tsx
+++ src/components/organisms/qna/FloatingButton.tsx
@@ -8,16 +8,35 @@ interface FloatingButtonProps {
   onClick: () => void;
 }
 
-const FloatingButton = forwardRef<HTMLButtonElement, FloatingButtonProps>(({isOpen, onClick}, ref) => {
+const FloatingButton = forwardRef<HTMLButtonElement, FloatingButtonProps>(
+  ({ isOpen, onClick }, ref) => {
 
-  const handleButtonClick = () => {
-    onClick();
-  };
-  return(
-    <button ref={ref} onClick={handleButtonClick} style={{ display:'block', alignContent:'center',width:'60px', height:'60px', borderRadius:'24px', backgroundColor:'#FF861A', border:'2px solid #ECEDEE4D', padding:'12px',boxSizing:'border-box',cursor:'pointer',zIndex:1000}}>
-      <Image style={isOpen?{height:'80%',width:'80%'}:{height:'100%',width:'100%'}} src={isOpen?FormOpen:FormClose} alt="button" />
-    </button>
-  )
-}
+    const handleButtonClick = () => {
+      onClick();
+    };
+    return (
+      <button ref={ref} onClick={handleButtonClick} style={{
+        display: 'block',
+        alignContent: 'center',
+        width: '60px',
+        height: '60px',
+        borderRadius: '24px',
+        backgroundColor: '#FF861A',
+        border: '2px solid #ECEDEE4D',
+        padding: '12px',
+        boxSizing: 'border-box',
+        cursor: 'pointer',
+        zIndex: 1000,
+      }}>
+        <Image
+          style={isOpen ?
+            { height: '80%', width: '80%' } :
+            { height: '100%', width: '100%' }}
+          src={isOpen ? FormOpen : FormClose}
+          alt="button"
+        />
+      </button>
+    );
+  },
 );
 export default FloatingButton;
