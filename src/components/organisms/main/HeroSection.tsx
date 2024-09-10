e4949d66b8c9c875c03a8c50aa183968c946dff0
cla6shade <cla6shade@github.com>
2024-09-10 17:00:31 +0900

    feat: 사용자 경험 개선
    애니메이션 적용 및 useLayoutEffect를 통한 fouc 해결

--- src/components/organisms/main/HeroSection.tsx
+++ src/components/organisms/main/HeroSection.tsx
@@ -1,33 +1,61 @@
-import backgroundImage from '@assets/images/hero-background.png';
 import Container from '@components/atoms/container/Container';
 import { Heading, TextBody } from '@components/atoms/text/TextFactory';
 import Text from '@components/atoms/text/Text';
-import { colors } from '@styles/colors';
 import Button from '@components/atoms/button/Button';
+import { keyframes } from '@emotion/react';
+import { ReactNode } from 'react';
+import backgroundImage from '@assets/images/hero-background.webp';
+import usePreloadBackground from '@/hooks/usePreloadBackground';
 
 function HeroSection() {
   return (
-    <Container size={{ width: '100%', height: '800px' }} style={{
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover',
-    }}
-      justify="center"
-      align="center"
-    >
+    <HeroContainer>
       <Container size="full-width" maxWidth="1210px" justify="center">
         <Container direction="column" align="center">
-          <Heading.XSmall color={colors.light.text.prominent}>부산대학교 대표 IT 프로젝트 동아리</Heading.XSmall>
+          <Heading.XSmall
+            css={{
+              opacity: 0,
+              animation: `${getLandingKeyframes(true)} 0.5s ease-in-out forwards 0.5s`,
+            }}
+          >
+            부산대학교 대표 IT 프로젝트 동아리
+          </Heading.XSmall>
           <HeroText />
-          <Button theme="light-outlined"  style={{
-            marginTop: '15px',
-          }}>
+          <Button
+            theme="light-outlined"
+            css={{
+              marginTop: '15px',
+              opacity: 0,
+              animation: `${getLandingKeyframes()} 0.5s ease-in-out forwards 0.5s`,
+            }}
+          >
             <TextBody.Large weight="bold">
               21기 모집 링크 바로가기
             </TextBody.Large>
           </Button>
         </Container>
       </Container>
+    </HeroContainer>
+  );
+}
 
+function HeroContainer({ children }: { children: ReactNode }) {
+  usePreloadBackground({ src: backgroundImage });
+  return (
+    <Container
+      size={{ width: '100%', height: '600px' }}
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+      responsiveStyle={{
+        sm: {
+          height: '800px',
+        },
+      }}
+      justify="center"
+      align="center">
+      {children}
     </Container>
   );
 }
@@ -40,10 +68,27 @@ function HeroText() {
       responsiveSize={{
         sm: '100px',
       }}
+      css={{
+        opacity: 0,
+        animation: `${getLandingKeyframes()} 0.5s ease-in-out forwards 0s`,
+      }}
     >
       APPTIVE
     </Text>
   );
+}
+
+function getLandingKeyframes(reverse: boolean = false) {
+  return keyframes`
+    from {
+      transform: translateY(${reverse ? '-20px' : '20px'});
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  `;
 }
 
 export default HeroSection;
