5e014cb2bc2a60dc0d6aa6e1ed061c5493d770cb
cla6shade <cla6shade@github.com>
2024-09-13 18:06:47 +0900

    refactor: 가독성을 위해 스타일 코드 분리

--- src/components/organisms/main/MainSection.tsx
+++ src/components/organisms/main/MainSection.tsx
@@ -13,9 +13,8 @@ import { breakPoints } from '@/styles/breakpoints';
 import boyIntro from '@assets/images/main/boyIntro.png';
 import girlIntro from '@assets/images/main/girlIntro.png';
 import Container from '@components/atoms/container/Container';
-import { ReactNode } from 'react';
+import { ReactNode, useRef } from 'react';
 import { MAX_CONTENT_WIDTH } from '@styles/sizes';
-
 interface MainSectionProps {
   mode: 'light' | 'dark';
 }
@@ -23,61 +22,16 @@ interface MainSectionProps {
 const MainSection = ({ mode }: MainSectionProps) => {
   const colorScheme = colors[mode];
 
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background: ${colorScheme.background.main};
-    padding: 0 35px;
-  `;
-  const Section = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 50px;
-
-    @media (max-width: ${breakPoints.md}) {
-      flex-direction: column;
-      gap: 30px;
-      text-align: center;
-    }
-  `;
-  const TextBox = styled.div`
-    margin-top: 80px;
-    margin-bottom: 50px;
-    display: flex;
-    max-width: ${MAX_CONTENT_WIDTH};
-    width: 100%;
-    text-align: left;
-    @media (min-width: ${breakPoints.sm}) {
-      margin-top: 200px;
-      margin-bottom: 50px;
-    }
-  `;
-  const Image = styled.img`
-    width: 250px;
-    height: 250px;
-
-    @media (max-width: ${breakPoints.sm}) {
-      width: 200px;
-      height: 200px;
-    }
-
-    @media (max-width: ${breakPoints.xs}) {
-      width: 150px;
-      height: 150px;
-    }
-  `;
-
-  const SectionReversed = styled(Section)`
-  @media (max-width: ${breakPoints.md}) {
-     flex-direction: column-reverse;
-  }
-`;
-
+  const spyRef = useRef<HTMLDivElement>(null);
 
   return (
-    <Wrapper>
+    <Container
+      padding="0 35px"
+      style={{ backgroundColor: colorScheme.background.main }}
+      align="center"
+      justify="center"
+      direction="column"
+    >
       <TextBox>
         <Heading.XLarge color={colorScheme.text.prominent}>
           What is Apptive?
@@ -85,7 +39,7 @@ const MainSection = ({ mode }: MainSectionProps) => {
       </TextBox>
       <MainTitleContainer>
         <Section>
-          <Image src={boyIntro} alt="boyIntro" />
+          <Image src={boyIntro} alt='boyIntro' />
           <IntroText mode={mode}>
             2013년에 개설된 APPTIVE는 학과 및 학년 제한없이 서비스 제작과 창업에
             관심있는 분들과 함께합니다. 기획자, 개발자, 디자이너로 구성된 팀으로
@@ -93,13 +47,21 @@ const MainSection = ({ mode }: MainSectionProps) => {
             전체 실력 향상을 목표로 합니다.
           </IntroText>
         </Section>
+
+        <div
+          ref={spyRef}
+          style={{
+            width: '100%',
+            height: '10px',
+          }}
+        />
         <SectionReversed>
           <IntroText mode={mode}>
             동아리 활동은 1년을 주기로 하며, 활동 이후 명예회원으로 전환하고
             동문회에 속하게 됩니다. 이후에도 현직자 네트워킹, 연합동아리 활동,
             테크 세미나 참석 등 자유로운 동아리 활동이 가능합니다.
           </IntroText>
-          <Image src={girlIntro} alt="girlIntro" />
+          <Image src={girlIntro} alt='girlIntro' />
         </SectionReversed>
       </MainTitleContainer>
       <TextBox>
@@ -285,15 +247,58 @@ const MainSection = ({ mode }: MainSectionProps) => {
           </YearTextBox>
         </SliderContainer>
       </SliderWrapper>
-    </Wrapper>
+    </Container>
   );
 };
 
+const Section = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 50px;
+
+    @media (max-width: ${breakPoints.md}) {
+      flex-direction: column;
+      gap: 30px;
+      text-align: center;
+    }
+  `;
+const TextBox = styled.div`
+    margin-top: 80px;
+    margin-bottom: 50px;
+    display: flex;
+    max-width: ${MAX_CONTENT_WIDTH};
+    width: 100%;
+    text-align: left;
+    @media (min-width: ${breakPoints.sm}) {
+      margin-top: 200px;
+      margin-bottom: 50px;
+    }
+  `;
+const Image = styled.img`
+    width: 250px;
+    height: 250px;
+
+    @media (max-width: ${breakPoints.sm}) {
+      width: 200px;
+      height: 200px;
+    }
+
+    @media (max-width: ${breakPoints.xs}) {
+      width: 150px;
+      height: 150px;
+    }
+  `;
+
+const SectionReversed = styled(Section)`
+  @media (max-width: ${breakPoints.md}) {
+     flex-direction: column-reverse;
+  }
+`;
 function MainTitleContainer({ children }: { children: ReactNode }) {
   return (
     <Container
       direction="column"
-      gap="60px"
+      gap="20px"
       maxWidth={MAX_CONTENT_WIDTH}
       size="full-width"
     >
