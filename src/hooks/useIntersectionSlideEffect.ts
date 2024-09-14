c6dc1702fd0e6925cd205ef51701f0d7ac565511
cla6shade <cla6shade@gmail.com>
2024-09-14 18:15:41 +0900

    feat: add intersection effect

---
+++ src/hooks/useIntersectionSlideEffect.ts
@@ -0,0 +1,28 @@
+import { RefObject, useCallback } from 'react';
+import useIntersectionEffect from '@/hooks/useIntersectionEffect';
+import { getLandingKeyframesArray } from '@/utils';
+
+interface UseIntersectionSlideEffectProps {
+  spyRef: RefObject<HTMLElement>;
+  targetRef: RefObject<HTMLElement>;
+  reverse?: boolean;
+  delay?: number;
+}
+
+function useIntersectionSlideEffect({ spyRef, targetRef, reverse, delay }: UseIntersectionSlideEffectProps) {
+  const effectCallback = useCallback(() => {
+    if (! targetRef.current)
+      return;
+
+    const keyframes = getLandingKeyframesArray();
+    targetRef.current.animate(keyframes, {
+      easing: 'ease-in-out',
+      duration: 500,
+      fill: 'forwards',
+      delay: delay || 0,
+    });
+  }, [targetRef, reverse]);
+  useIntersectionEffect({ spyRef, threshold: 0.8, callback: effectCallback, triggerOnce: true });
+}
+
+export default useIntersectionSlideEffect;
