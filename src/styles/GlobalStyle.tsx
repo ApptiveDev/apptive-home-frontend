769f20f2ba54e71f97fedca50d3be852c549f69b
harugi7 <gimme2721@gmail.com>
2024-09-16 01:10:17 +0900

    chore: fix scroll bar

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -9,7 +9,8 @@ const baseStyle = css`
   }
   body {
     font-family: 'Pretendard Variable', sans-serif;
-    overflow-y: scroll;
+    scrollbar-color: rgba(25, 27, 28) rgba(255, 255, 255);
+    scrollbar-width: thin; 
   }
 
   ::-webkit-scrollbar {
@@ -23,11 +24,6 @@ const baseStyle = css`
 
   ::-webkit-scrollbar-track {
     background: rgba(255, 255, 255);
-  }
-
-  body {
-    scrollbar-color: rgba(25, 27, 28) rgba(255, 255, 255);
-    scrollbar-width: thin; 
   }
 
 `;
